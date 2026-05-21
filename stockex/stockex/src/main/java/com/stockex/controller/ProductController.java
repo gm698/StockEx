a88d92eae530940fd7fc115c925569b21d09f2c9
Geo Murray <gm698@drexel.edu>
2026-05-21 03:15:01 -0400

    Milestone 1
    Initial Spring Boot and Oracle product API implementation.

---
+++ stockex/stockex/src/main/java/com/stockex/controller/ProductController.java
@@ -0,0 +1,33 @@
+package com.stockex.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.*;
+
+import com.stockex.model.Product;
+import com.stockex.service.ProductService;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+	private final ProductService productService;
+
+	public ProductController(ProductService productService) {
+		this.productService = productService;
+	}
+
+	@GetMapping
+	public List<Product> getAllProducts() {
+		return productService.getAllProducts();
+	}
+
+	@GetMapping("/{id}")
+	public Product getProductById(@PathVariable Long id) {
+		return productService.getProductById(id);
+	}
+
+	@PostMapping
+	public Product createProduct(@RequestBody Product product) {
+		return productService.createProduct(product);
+	}
+}
